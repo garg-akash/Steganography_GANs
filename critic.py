9a79e16c3a6bfe4c0f38b96d8b27d9d23ae648ca
garg-akash <akashgrg2@gmail.com>
2020-02-26 03:53:05 +0530

    decoder and critic added

---
+++ critic.py
@@ -0,0 +1,58 @@
+import torch
+from torch import nn
+
+
+class BasicCritic(nn.Module):
+    """
+    The BasicCritic module takes an image and predicts whether it is a cover
+    image or a steganographic image (N, 1).
+
+    Input: (N, 3, H, W)
+    Output: (N, 1)
+    """
+
+    def _conv2d(self, in_channels, out_channels):
+        return nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3
+        )
+
+    def _build_models(self):
+
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )  
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size, 1)
+        )         
+
+        return self.conv1,self.conv2,self.conv3,self.conv4
+
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self._models = self._build_models()
+
+    def forward(self, image):
+        x = self._models[0](image)
+        x_list = [x]
+        x_1 = self._models[1](torch.cat(x_list, dim=1))
+        x_list.append(x_1)
+        x_2 = self._models[2](torch.cat(x_list, dim=1))
+        x_list.append(x_2)
+        x_3 = self._models[3](torch.cat(x_list, dim=1))
+        return torch.mean(x_3.view(x_3.size(0), -1), dim=1)
+        
---
+++ decoder.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+
+
+class BasicDecoder(nn.Module):
+    """
+    The BasicDecoder module takes an steganographic image and attempts to decode
+    the embedded data tensor.
+
+    Input: (N, 3, H, W)
+    Output: (N, D, H, W)
+    """
+
+    def _conv2d(self, in_channels, out_channels):
+        return nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+    def _build_models(self):
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )  
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.data_depth)
+        )         
+
+        return self.conv1,self.conv2,self.conv3,self.conv4
+
+    def __init__(self, data_depth, hidden_size):
+        super().__init__()
+        self.data_depth = data_depth
+        self.hidden_size = hidden_size
+        self._models = self._build_models()
+
+    def forward(self, image):
+        x = self._models[0](image)
+        x_list = [x]
+        x_1 = self._models[1](torch.cat(x_list, dim=1))
+        x_list.append(x_1)
+        x_2 = self._models[2](torch.cat(x_list, dim=1))
+        x_list.append(x_2)
+        x_3 = self._models[3](torch.cat(x_list, dim=1))
+        x_list.append(x_3)
+        return x_3
+
+
+
+class DenseDecoder(BasicDecoder):
+
+    def _build_models(self):
+        self.conv1 = super()._models[0]
+        self.conv2 = super()._models[1]
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size * 2, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size)
+        )
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size * 3, self.data_depth)
+            )
+
+        return self.conv1, self.conv2, self.conv3, self.conv4
+
