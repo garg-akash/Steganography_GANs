b690a7294022e48aa8effaebf42a1e8f8666448e
garg-akash <akashgrg2@gmail.com>
2020-02-25 21:36:43 +0530

    Encoder added

---
+++ encoder_AS.py
@@ -0,0 +1,86 @@
+import torch
+from torch import nn
+
+
+class BasicEncoder(nn.Module):
+    """
+    The BasicEncoder module takes an cover image and a data tensor and combines
+    them into a steganographic image.
+
+    """
+
+    def _conv2d(self, in_channels, out_channels):
+        return nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+    def _build_models(self):
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size, 3),
+        )
+        return self.conv1, self.conv2, self.conv3, self.conv4 
+
+    def __init__(self, data_depth, hidden_size):
+        super().__init__()
+        self.data_depth = data_depth
+        self.hidden_size = hidden_size
+        self._models = self._build_models()
+
+    def forward(self, image, data):
+        x = self._models[0](image)
+        x_list = [x]
+        x_1 = self._models[1](torch.cat(x_list + [data], dim=1))
+        x_list.append(x_1)
+        x_2 = self._models[2](torch.cat(x_list + [data], dim=1))
+        x_list.append(x_2)
+        x_3 = self._models[3](torch.cat(x_list + [data], dim=1))
+        x_list.append(x_3)
+        return x_3
+
+
+class ResidualEncoder(BasicEncoder):
+
+    def forward(self, image, data):
+        return image + super().forward(self, image, data)
+        
+class DenseEncoder(ResidualEncoder):
+
+    def _build_models(self):
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size * 2 + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size * 3 + self.data_depth, 3)
+        )
+
+        return self.conv1, self.conv2, self.conv3, self.conv4
